5a20e0ece099b88177610aa76116fa45f73d2d4b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 23:11:40 -0300

    sei la

--- terraform/modules/network/output.tf
+++ terraform/modules/network/output.tf
@@ -1,13 +1,11 @@
-output "subnet_pub_id" {
-  value = aws_subnet.sn_public.id
-}
-
-output "sg_public_id" {
-  value = aws_security_group.sg_public.id
-}
-
-# NETWORK OUTPUT TO BE REUSED
-
 output "vpc_id" {
     value = "${aws_vpc.vpc.id}"
+}
+
+output "vpc_sg_pub_id" {
+    value = "${aws_security_group.vpc_sg_pub.id}"
+}
+
+output "vpc_sn_pub_az1_id" {
+    value = "${aws_subnet.sn_pub_az1.id}"
 }
