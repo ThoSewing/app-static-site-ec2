6986b6d04e2a3708925f0020ee405479dcb0f879
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 21:33:20 -0300

    Cp 1: correção do provider 2

--- terraform/modules/provider.tf
+++ terraform/modules/provider.tf
@@ -1,20 +1,20 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.4"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.55"
-    }
-  }
+  required_version = "~> 1.5"
 
   backend "s3" {
     bucket         = "tf-static-state-thomas-fernandes"
     key            = "terraform.tfstate"
     dynamodb_table = "tf-static-state-thomas-fernandes"
     region         = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 
 }
