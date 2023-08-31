6cd50be9894c76f3edff47e57aec48663947bc69
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 21:13:46 -0300

    Cp 1

---
+++ terraform/modules/compute/compute.tf
@@ -0,0 +1,18 @@
+# RESOURCE: EC2 LAUNCH TEMPLATE
+
+data "template_file" "user_data" {
+    template = "${file("./modules/compute/scripts/user_data.sh")}"
+    vars = {
+        access_key_id     = "${var.access_key_id}"
+        secret_access_key = "${var.secret_access_key}"
+        session_token     = "${var.session_token}"
+    }
+}
+
+resource "aws_launch_template" "ec2_lt" {
+    name                   = "${var.ec2_lt_name}"
+    image_id               = "${var.ec2_lt_ami}"
+    instance_type          = "${var.ec2_lt_instance_type}"
+    user_data              = "${base64encode(data.template_file.user_data.rendered)}"
+    vpc_security_group_ids = ["${var.vpc_sg_pub_id}"]
+}
---
+++ terraform/modules/compute/vars.tf
@@ -0,0 +1,24 @@
+# NETWORK VARS DEFAULT VALUES (INPUT IS REQUIRED BECAUSE NO DEFAULT IS DEFINED)
+
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "vpc_sn_pub_az1_id" {}
+variable "vpc_sg_pub_id" {}
+
+
+# COMPUTE VARS DEFAULT VALUES
+
+variable "ec2_lt_name" {
+    type    = string
+    default = "ec2_lt_name"
+}
+
+variable "ec2_lt_ami" {
+    type    = string
+    default = "ami-02e136e904f3da870"
+}
+
+variable "ec2_lt_instance_type" {
+    type    = string
+    default = "t2.micro"
+}
