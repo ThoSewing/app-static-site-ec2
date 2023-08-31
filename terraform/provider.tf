bcb0710390ac0decbdc720e013542640a98c4bc1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 23:26:32 -0300

    teste

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,12 +3,6 @@ terraform {
 
   required_version = "~> 1.5"
 
-  backend "s3" {
-    bucket         = "tf-static-state-thomas-fernandes"
-    key            = "dev/terraform.tfstate"
-    dynamodb_table = "tf-static-state-thomas-fernandes"
-    region         = "us-east-1"
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
